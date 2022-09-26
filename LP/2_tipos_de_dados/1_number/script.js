83909b19c38c1d8954edc939f3f6887f8fb79b87
paulaLH <112903358+paulaLH@users.noreply.github.com>
2022-09-26 17:06:32 -0300

    Add files via upload

--- LP/2_tipos_de_dados/1_number/script.js
+++ LP/2_tipos_de_dados/1_number/script.js
@@ -1,1 +1,10 @@
 console.log('number');
+console.log(typeof 5);
+
+console.log(typeof 12.5);
+
+console.log(typeof -3);
+
+console.log(typeof "5");
+
+console.log(typeof true);
---
+++ LP/2_tipos_de_dados/2_aritmetica/script.js
@@ -0,0 +1,10 @@
+console.log (2+3);
+
+console.log (5-3);
+console.log (5/3);
+
+console.log (5*3);
+
+console.log(21 % 2);
+
+console.log(typeof (2%2);
